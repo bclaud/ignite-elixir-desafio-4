f4b9d43c9f62a748140d1a624bc8bfd57b9247f3
bclaud <bruarrais@gmail.com>
2022-05-18 17:39:23 -0300

    add user struct

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -3,7 +3,8 @@ defmodule Flightex.Users.User do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(%{name: _name, email: _email, cpf: _cpf} = params) do
+    params = Map.put(params, :id, UUID.uuid4())
+    struct(__MODULE__, params)
   end
 end
